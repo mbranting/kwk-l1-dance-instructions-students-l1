7a75cc99c75eb73e0b8fd8e3df604602104ff7b2
mbranting <mckenna.branting@gmail.com>
2018-06-05 23:32:11 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,23 @@
 
 # Code your methods below
+def assume_stance
+  Lower_Body = "plant legs far apart, bend knees slightly and keep posture loose"
+  puts "1.#{Lower_Body}"
+end 
+assume_stance
+
+def bust_move
+  puts "Be cool"
+  puts "Be snazzy"
+end
+bust_move
+
+def footwork
+  puts "1. Lift right foot"
+  puts "2. Return right foot to the ground"
+  puts "3. Finishing with a small skip-step backward"
+end
+footwork
+
+def bob_the_reins
+  put
